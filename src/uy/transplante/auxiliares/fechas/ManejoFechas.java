760cabd8108d54347ef45e6f0950c735b87dd8c3
rsantellan <rsantellan@435675c6-14bd-11de-896d-b39e94f1936c>
2010-09-22 22:15:54 +0000

    refactoring

--- src/uy/transplante/auxiliares/fechas/ManejoFechas.java
+++ src/uy/transplante/auxiliares/fechas/ManejoFechas.java
@@ -1,14 +1,13 @@
 package uy.transplante.auxiliares.fechas;
 
 import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 public final class ManejoFechas {
-	public final static SimpleDateFormat FORMATOINGLES = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-	public final static SimpleDateFormat FORMATOESPANOL = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
-	public final static SimpleDateFormat FORMATOHORAS = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
-	public final static SimpleDateFormat FORMATOMES = new SimpleDateFormat("MMMMM", Locale.getDefault());
-	public final static SimpleDateFormat FORMATOYEAR = new SimpleDateFormat("yyyy", Locale.getDefault());
+	public final static SimpleDateFormat FORMATOINGLES = new SimpleDateFormat("yyyy-MM-dd");
+	public final static SimpleDateFormat FORMATOESPANOL = new SimpleDateFormat("dd-MM-yyyy");
+	public final static SimpleDateFormat FORMATOHORAS = new SimpleDateFormat("HH:mm:ss");
+	public final static SimpleDateFormat FORMATOMES = new SimpleDateFormat("MMMMM");
+	public final static SimpleDateFormat FORMATOYEAR = new SimpleDateFormat("yyyy");
 
 	
 }

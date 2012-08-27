110b06dcf9d267c0cb5d8ce692444281bbd69eb4
rsantellan@gmail.com <rsantellan@gmail.com@435675c6-14bd-11de-896d-b39e94f1936c>
2012-08-27 15:04:42 +0000

    Se commitea roto

--- lib/model/trasplanteComplicacionesConsultaBehaviorListener.php
+++ lib/model/trasplanteComplicacionesConsultaBehaviorListener.php
@@ -4,29 +4,25 @@ class trasplanteComplicacionesConsultaBehaviorListener extends Doctrine_Record_L
 {  
 	public function postSave(Doctrine_Event $event) 
 	{
-            $object = $event->getInvoker();
-            if(!Doctrine::getTable('mdContent')->retrieveByObject($object)){
-                $mdContent = new MdContent();
-                $mdContent->setObjectClass(get_class($object));
-                $mdContent->setObjectId($object->getId());
-                $mdContent->setMdUserId($object->getMdUserIdTmp());
-                $mdContent->save();
-            }
+      /*
+      $object = $event->getInvoker();
+      if(!Doctrine::getTable('mdContent')->retrieveByObject($object)){
+          $mdContent = new MdContent();
+          $mdContent->setObjectClass(get_class($object));
+          $mdContent->setObjectId($object->getId());
+          $mdContent->setMdUserId($object->getMdUserIdTmp());
+          $mdContent->save();
+      }
+      */
 	}
 	
 	public function preDelete(Doctrine_Event $event)
-  {
-            
-            $object = $event->getInvoker();
-            //$list = $object->retrieveContents();
-            $mdContent = Doctrine::getTable('mdContent')->retrieveByObject($object);
-            Doctrine::getTable('mdContentRelation')->deleteRelatedOfId($mdContent->getId());
-            /*foreach($list as $aux)
-            {
-              $aux->delete();
-            }
-            $content = Doctrine::getTable('mdContent')->retrieveByObject($object);
-            $content->delete();*/
+    {
+      /*    
+      $object = $event->getInvoker();
+      $mdContent = Doctrine::getTable('mdContent')->retrieveByObject($object);
+      Doctrine::getTable('mdContentRelation')->deleteRelatedOfId($mdContent->getId());
+      */
 	}	
 }
 

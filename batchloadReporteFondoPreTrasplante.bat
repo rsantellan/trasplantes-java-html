6dad03f75ed2ad84724fa2c76612dcc4b4a9e92a
rsantellan@gmail.com <rsantellan@gmail.com@435675c6-14bd-11de-896d-b39e94f1936c>
2012-08-31 19:14:08 +0000

    Se agrega un batch para actualizar los reportes.

---
+++ batchloadReporteFondoPreTrasplante.bat
@@ -0,0 +1,28 @@
+cd C:\trasplantes
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2012" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2011" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2010" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2009" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2008" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2007" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2006" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2005" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2004" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2003" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2002" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2001" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2000" --trace
+php symfony loading:reporteFondoRACMV --year="2000" --yearFrom="2000" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2001" --yearFrom="2012" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2002" --yearFrom="2011" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2003" --yearFrom="2010" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2004" --yearFrom="2009" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2005" --yearFrom="2008" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2006" --yearFrom="2007" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2007" --yearFrom="2006" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2008" --yearFrom="2005" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2009" --yearFrom="2004" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2010" --yearFrom="2003" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2011" --yearFrom="2002" --completo="true" --trace
+php symfony loading:reporteFondoRACMV --year="2012" --yearFrom="2001" --completo="true" --trace
+
